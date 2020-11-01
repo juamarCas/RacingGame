135f74598d78e33f56b32007011dbc0fad245341
MexxaDev <nahuelbeeguer999@gmail.com>
2020-11-01 15:43:07 -0300

    PowerUp

---
+++ Assets/Scripts/PowerUp/ManagerPowerUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerPowerUp : MonoBehaviour
+{
+    float timer;
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, 8f);
+    }
+
+    void OnTriggerEnter(Collider PowerUp)
+    {
+        if (PowerUp.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PowerUp/PwProyectil.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PwProyectil : MonoBehaviour
+{
+    void Update()
+    {
+        Destroy(gameObject, 8f);
+        gameObject.transform.Translate(new Vector3(0f, 0f, 0.5f));
+    }
+}
