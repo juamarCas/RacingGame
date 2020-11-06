b726689b6c4caf1c1093a677c43eac1924f87360
Juan Martin <juanmartincas@gmail.com>
2020-11-05 20:09:11 -0500

    Cam

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    [Header ("Numero de Checkpoint")]
+    public int Numero_Checkpoint; //DETERMINA EL NUMERO DE CHECKPOINT QUE ES.
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ///PASO 1: VERIFICAR QUE LO QUE COLISIONA ES UN VEHICULO, YA QUE POR ERROR PUEDE COLISIONAR
+        //OTRA COSA POR EJEMPLO UNA BALA LANZADA, PARTE DEL ESCENARIO SI ES MOVIL, ETC !
+
+        if (other.gameObject.GetComponent<VerificadorVuelta>() != null)
+        {
+            //Debug.Log("ok");
+            //SI ES UN COCHE LO QUE ME COLISIONO, ENTONCES:
+
+            //PASO 2: VERIFICO QUE EL ANTERIOR CHECKPOINT ESTE HECHO, SINO NO PUEDO DARLE MI OK.
+            if (Numero_Checkpoint == 0) //pero si soy el cero si o si le asigno.
+            {
+                other.gameObject.GetComponent<VerificadorVuelta>().CheckPoints[Numero_Checkpoint] = true;
+            }
+            else
+            {
+                if (other.gameObject.GetComponent<VerificadorVuelta>().CheckPoints[Numero_Checkpoint - 1] == true)
+                {
+                    other.gameObject.GetComponent<VerificadorVuelta>().CheckPoints[Numero_Checkpoint] = true;
+                }
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/VerificadorVuelta.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerificadorVuelta : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool[] CheckPoints;
+
+    private void Start()
+    {
+        //AQUI SE DEBE PONER LA CANTIDAD QUE TENGA CADA PISTA.
+        //TEMPORALMENTE PONDREMOS 3 CHECPOINTS
+        CheckPoints = new bool[3];
+    }
+
+
+
+}
