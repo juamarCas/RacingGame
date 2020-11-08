c63c943336c379835ed3150c88123064f332447a
Claudio-San <grenierclaudio@hotmail.com>
2020-11-08 00:01:57 -0300

    VUELTAS3

---
+++ Assets/Scripts/APAGAR1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class APAGAR1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool ApagarAqui;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ApagarAqui == true) this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/mouseon.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mouseon : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler
+{
+    public GameObject cuadrado, Okey, Bipi;
+    public GameObject Contenedor2;
+     
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        cuadrado.transform.position = this.transform.position;
+        Bipi.GetComponent<AudioSource>().Play();
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        if (this.gameObject.name == "JUGAR")
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Okey.GetComponent<AudioSource>().Play();
+                Contenedor2.SetActive(true);
+                APAGAR1.ApagarAqui = true;
+            }
+            
+            //if (Input.GetMouseButtonDown(1)) Debug.Log("Pressed right click.");
+            //if (Input.GetMouseButtonDown(2)) Debug.Log("Pressed middle click.");
+        }
+        if (this.gameObject.name == "OK_PERSONAJE")
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene("TestRoom");
+            }
+
+            //if (Input.GetMouseButtonDown(1)) Debug.Log("Pressed right click.");
+            //if (Input.GetMouseButtonDown(2)) Debug.Log("Pressed middle click.");
+        }
+    }
+}
